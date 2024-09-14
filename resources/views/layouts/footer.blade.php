be570b968323e002cb55ba8e204178fe66c67d25
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-09-14 12:25:56 +0100

    Work on footer and welcome page

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,17 +1,8 @@
 <footer>
-    <div class="container">
+    <div class="m-0">
         <div class="footer-content">
             <div class="year">{{ $gitTag }} &copy; {{ date('Y') }} HRMS.</div>
             {{ \Carbon\Carbon::now()->setTimezone(config('app.timezone'))->format('d/m/Y H:i') }}
-            <div class="languages">
-                <span>Available languages:</span>
-                <ul class="horizontal-list">
-                    <li><x-flag-country-gb width="24" height="24"/></li>
-                    <li><x-flag-country-pt width="24" height="24"/></li>
-                    <li><x-flag-country-es width="24" height="24"/></li>
-                    <li><x-flag-language-cy width="24" height="24"/></li>
-                </ul>
-            </div>
         </div>
     </div>
 </footer>
