b23410e939248e91c392b5cbadc647f7295dd12e
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-09-17 18:46:14 +0100

    Finish off localising timezone depending on logged in user

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,8 +2,7 @@
     <div class="m-0">
         <div class="footer-content">
             <div class="year">{{ $gitTag }} &copy; {{ date('Y') }} HRMS.</div>
-            {{ \Carbon\Carbon::now()->setTimezone(config('app.timezone'))->format('d/m/Y H:i') }}
-            {{ \Carbon\Carbon::now()->setTimezone(Auth::user()->timezone)->format('d/m/Y H:i') }}
+            {{ \Carbon\Carbon::now()->setTimezone(Auth::user()->timezone ?? config('app.timezone'))->format('d/m/Y H:i') }}
         </div>
     </div>
 </footer>
