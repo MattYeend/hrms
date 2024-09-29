4e5a75473cdec956a77043fc90b2b613daa7dc2a
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-09-29 13:22:14 +0100

    #3 Updates to web.php routes file

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\LocaleController;
+use App\Http\Controllers\LeaveController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     if (Auth::check()) {
@@ -13,14 +16,14 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/show/{id}', [App\Http\Controllers\UserController::class, 'show'])->name('profile');
-Route::get('/get-dark-mode-preference', [App\Http\Controllers\UserController::class, 'getDarkModePreference'])->name('get-dark-mode-preference');
-Route::post('/toggle-dark-mode', [App\Http\Controllers\UserController::class, 'toggleDarkMode'])->name('toggle-dark-mode');
+Route::get('/show/{id}', [UserController::class, 'show'])->name('profile');
+Route::get('/get-dark-mode-preference', [UserController::class, 'getDarkModePreference'])->name('get-dark-mode-preference');
+Route::post('/toggle-dark-mode', [UserController::class, 'toggleDarkMode'])->name('toggle-dark-mode');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/super-admin-home', [App\Http\Controllers\HomeController::class, 'superAdminHome'])->name('super-admin-home');
-Route::get('/admin-home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin-home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/super-admin-home', [HomeController::class, 'superAdminHome'])->name('super-admin-home');
+Route::get('/admin-home', [HomeController::class, 'adminHome'])->name('admin-home');
 
 Route::post('/locale', LocaleController::class)->name('locale.change');
 
-Route::get('/calendar', [App\Http\Controllers\LeaveController::class, 'index'])->name('calendar');
+Route::get('/calendar', [LeaveController::class, 'index'])->name('calendar');
