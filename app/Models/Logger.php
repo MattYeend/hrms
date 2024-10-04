6753c2e2a69d4771b19c6820990f887890df76a5
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-10-04 18:05:39 +0100

    #20 Added log function in logger model

--- app/Models/Logger.php
+++ app/Models/Logger.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Logger extends Model
 {
@@ -43,4 +44,24 @@ class Logger extends Model
     const ACTION_CREATE_JOB = 15;
     const ACTION_UPDATE_JOB = 16;
     const ACTION_DELETE_JOB = 17;
+
+    public static function log($action = 0, $data = null, $logged_in_user_id = null, $related_to_user_id = null){
+        if(isset($action)){
+            if(empty($logged_in_user_id)){
+                $logged_in_user_id = Auth::user()->id;
+            }
+            if(in_array(gettype($data), ['array'])){
+                $data = json_encode($data);
+            }else if(!is_null($data)){
+                throw new \InvalidArgumentException("Data must be an array or null.");
+            }
+
+            $log = new Logger;
+            $log->logged_in_user_id = $logged_in_user_id;
+            $log->action = $action;
+            $log->related_to_user_id = $related_to_user_id;
+            $log->data = $data;
+            $log->save();
+        }
+    }
 }
