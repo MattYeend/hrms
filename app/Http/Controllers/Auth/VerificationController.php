62b9f972a3ddf4633d529bb6b006e94f635048c2
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-10-05 11:58:43 +0100

    #26 Finished adding logger log to auth controllers

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Models\Logger;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Http\Request;
 
 class VerificationController extends Controller
 {
@@ -38,5 +39,16 @@ class VerificationController extends Controller
         $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
+    }
+
+    /**
+     * Override the verify method to log the action
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function verify(Request $request){
+        Logger::log(Logger::ACTION_USER_VERIFICATION);
+        return $this->verified($request);
     }
 }
