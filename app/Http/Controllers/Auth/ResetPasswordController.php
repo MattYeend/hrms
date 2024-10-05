7daa5e75e67e403a29d4af081a6018016bce7652
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-10-05 11:56:25 +0100

    #26 Added log to reset password controller

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Models\Logger;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -27,4 +28,21 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = '/home';
+
+    /**
+     * Override resetPassword to log the action
+     * 
+     * @param \App\Models\User $user
+     * @param string $password
+     * @return void
+     */
+    protected function resetPassword($user, $password){
+        Logger::log(Logger::ACTION_RESET_PASSWORD);
+
+        $user->password = Hash::make($password);
+        $user->setRememberToken(Str::random(60));
+        $user->save();
+
+        $this->guard()->login($user);
+    }
 }
