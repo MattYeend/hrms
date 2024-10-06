9cb575f64da987d8962a78a55495a3f7f306d216
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-10-06 13:24:50 +0100

    #5 Update to leave form styling

--- resources/views/leave/edit.blade.php
+++ resources/views/leave/edit.blade.php
@@ -15,17 +15,19 @@
                             @method('PUT')
                         @endif
 
-                        <div class="form-group">
-                            <label for="date_from">{{ __('leave.date_from') }}</label>
-                            <input type="date" name="date_from" id="date_from" class="form-control" value="{{ old('date_from', isset($leave) ? $leave->date->from : '') }}">
+                        <div class="row">
+                            <div class="form-group col-md-3">
+                                <label for="date_from">{{ __('leave.date_from') }}</label>
+                                <input type="date" name="date_from" id="date_from" class="form-control" value="{{ old('date_from', isset($leave) ? $leave->date->from : '') }}">
+                            </div>
+
+                            <div class="form-group col-md-3">
+                                <label for="date_to">{{ __('leave.date_to') }}</label>
+                                <input type="date" name="date_to" id="date_to" class="form-control" value="{{ old('date_to', isset($leave) ? $leave->date->to : '') }}">
+                            </div>
                         </div>
 
-                        <div class="form-group">
-                            <label for="date_to">{{ __('leave.date_to') }}</label>
-                            <input type="date" name="date_to" id="date_to" class="form-control" value="{{ old('date_to', isset($leave) ? $leave->date->to : '') }}">
-                        </div>
-
-                        <div class="form-group">
+                        <div class="form-group col-md-4">
                             <label for="leave_type_id">{{ __('leave.leave_type') }}</label>
                             <select name="leave_type_id" id="leave_type_id" class="form-control">
                                 @foreach($leaveTypes as $leaveType)
@@ -36,14 +38,16 @@
                             </select>
                         </div>
 
-                        <div class="form-group">
-                            <label for="half_day_am">{{ __('leave.half_day_am') }}</label>
-                            <input type="checkbox" name="half_day_am" id="half_day_am" value="1" {{ old('half_day_am', isset($leave) && leave->half_day_am ? 'checked' : '') }}>
-                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-3">
+                                <label for="half_day_am">{{ __('leave.half_day_am') }}</label>
+                                <input type="checkbox" name="half_day_am" id="half_day_am" value="1" {{ old('half_day_am', isset($leave) && leave->half_day_am ? 'checked' : '') }}>
+                            </div>
 
-                        <div class="form-group">
-                            <label for="half_day_pm">{{ __('leave.half_day_pm') }}</label>
-                            <input type="checkbox" name="half_day_pm" id="half_day_pm" value="1" {{ old('half_day_pm', isset($leave) && leave->half_day_pm ? 'checked' : '') }}>
+                            <div class="form-group col-md-3">
+                                <label for="half_day_pm">{{ __('leave.half_day_pm') }}</label>
+                                <input type="checkbox" name="half_day_pm" id="half_day_pm" value="1" {{ old('half_day_pm', isset($leave) && leave->half_day_pm ? 'checked' : '') }}>
+                            </div>
                         </div>
 
                         <button type="submit" class="btn btn-primary">{{ __('leave.update_holiday') }}</button>
