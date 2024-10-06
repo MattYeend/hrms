3939f616e2f20613ae3ca99378d1d4f5754c9150
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-10-06 10:37:18 +0100

    #5 Added middleware to web routes

--- routes/web.php
+++ routes/web.php
@@ -16,23 +16,25 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/show/{id}', [UserController::class, 'show'])->name('profile');
-Route::get('/get-dark-mode-preference', [UserController::class, 'getDarkModePreference'])->name('get-dark-mode-preference');
-Route::post('/toggle-dark-mode', [UserController::class, 'toggleDarkMode'])->name('toggle-dark-mode');
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/super-admin-home', [HomeController::class, 'superAdminHome'])->name('super-admin-home');
-Route::get('/admin-home', [HomeController::class, 'adminHome'])->name('admin-home');
-
 Route::post('/locale', LocaleController::class)->name('locale.change');
 
-Route::get('/calendar', [LeaveController::class, 'index'])->name('calendar');
-Route::get('/api/holidays', [LeaveController::class, 'getHolidays']);
-Route::get('/leaves/create', [LeaveController::class, 'create'])->name('leave.create');
-Route::post('/leaves', [LeaveController::class, 'store'])->name('leave.store');
-Route::get('/leave/{leave}', [LeaveController::class, 'show'])->name('leave.show');
-Route::get('/leave/{leave}/edit', [LeaveController::class, 'edit'])->name('leave.edit');
-Route::put('/leave/{leave}', [LeaveController::class, 'update'])->name('leave.update');
-Route::delete('/leave/{leave}', [LeaveController::class, 'delete'])->name('leave.delete');
-Route::post('/leave/{leave}/approve', [LeaveController::class, 'approve'])->name('leave.approve')->middleware('can:approve, leave');
-Route::post('/leave/{leave}/deny', [LeaveController::class, 'deny'])->name('leave.deny')->middleware('can:deny, leave');
+Route::middleware(['auth'])->group(function(){
+    Route::get('/show/{id}', [UserController::class, 'show'])->name('profile');
+    Route::get('/get-dark-mode-preference', [UserController::class, 'getDarkModePreference'])->name('get-dark-mode-preference');
+    Route::post('/toggle-dark-mode', [UserController::class, 'toggleDarkMode'])->name('toggle-dark-mode');
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/super-admin-home', [HomeController::class, 'superAdminHome'])->name('super-admin-home');
+    Route::get('/admin-home', [HomeController::class, 'adminHome'])->name('admin-home');
+
+    Route::get('/calendar', [LeaveController::class, 'index'])->name('calendar');
+    Route::get('/api/holidays', [LeaveController::class, 'getHolidays']);
+    Route::get('/leaves/create', [LeaveController::class, 'create'])->name('leave.create');
+    Route::post('/leaves', [LeaveController::class, 'store'])->name('leave.store');
+    Route::get('/leave/{leave}', [LeaveController::class, 'show'])->name('leave.show');
+    Route::get('/leave/{leave}/edit', [LeaveController::class, 'edit'])->name('leave.edit');
+    Route::put('/leave/{leave}', [LeaveController::class, 'update'])->name('leave.update');
+    Route::delete('/leave/{leave}', [LeaveController::class, 'delete'])->name('leave.delete');
+    Route::post('/leave/{leave}/approve', [LeaveController::class, 'approve'])->name('leave.approve')->middleware('can:approve, leave');
+    Route::post('/leave/{leave}/deny', [LeaveController::class, 'deny'])->name('leave.deny')->middleware('can:deny, leave');
+});
