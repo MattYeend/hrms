94457b8b322f14ff4085b516119e22f387fef3ec
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-10-20 15:19:37 +0100

    #64 New enum class for rsvp status

---
+++ app/Enums/RsvpStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum RsvpStatus: string
+{
+    case ATTEND = 'attend';
+    case MAYBE = 'maybe';
+    case DECLINE = 'decline';
+    case OUTSTANDING = 'outstanding';
+}
