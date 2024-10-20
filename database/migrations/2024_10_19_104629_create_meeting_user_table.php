f628e1757681486f14422ebef1e3754f2415a4fe
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-10-20 15:16:42 +0100

    #64 New field in meetinguser pivot table

--- database/migrations/2024_10_19_104629_create_meeting_user_table.php
+++ database/migrations/2024_10_19_104629_create_meeting_user_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id')->nullable();
             $table->unsignedBigInteger('meeting_id')->nullable();
+            $table->enum('rsvp_status', ['attend', 'maybe', 'decline', 'outstanding'])->default('outstanding');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
