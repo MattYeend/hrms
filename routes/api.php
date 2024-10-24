d0dfde1ecdc3d6fd9e1d7433305c60c9b7ae9793
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-10-24 08:09:32 +0100

    #74 Added middleware to api routes

--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/locale', LocaleController::class)->name('locale.change');
+Route::post('/locale', LocaleController::class)->middleware('auth:sanctum')->name('locale.change');
 
-Route::get('/api/holidays', [LeaveController::class, 'getHolidays']);
-Route::get('/api/leaves', [LeaveController::class, 'getLeaves']);
+Route::get('/api/holidays', [LeaveController::class, 'getHolidays'])->middleware('auth:sanctum');
+Route::get('/api/leaves', [LeaveController::class, 'getLeaves'])->middleware('auth:sanctum');
 
-Route::get('/api/meetings', [MeetingsController::class, 'getMeetings'])->name('meetings.get');
+Route::get('/api/meetings', [MeetingsController::class, 'getMeetings'])->middleware('auth:sanctum')->name('meetings.get');
