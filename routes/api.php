88b984e9f993d76e08361b2b49d24fbee922bdb8
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-10-24 08:03:22 +0100

    74 Start of moving api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LocaleController;
+use App\Http\Controllers\LeaveController;
+use App\Http\Controllers\MeetingsController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
