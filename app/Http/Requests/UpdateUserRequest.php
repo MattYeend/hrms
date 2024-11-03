bb9542cccef8a9a97016289248d3cb045cf62a5b
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-11-03 09:57:52 +0000

    #9 Update to store and update user requests

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class UpdateUserRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UpdateUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return Auth::user() && (Auth::user()->isAdmin() || Auth::user()->isSuperAdmin());
     }
 
     /**
