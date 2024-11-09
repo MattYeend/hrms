5592d879a4cd6f57d042713aaff0acfe06cf08f4
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-11-09 13:15:44 +0000

    #105 Update to profile page

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -13,25 +13,43 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    <div class="float-left">
-                        <p>{{ __('users.first_line') }}: {{ $user->first_line }}</p>
-                        <p>{{ __('users.second_line') }}: {{ $user->second_line?:'Not Provided' }}</p>
-                        <p>{{ __('users.town') }}: {{ $user->town?:'Not Provided' }}</p>
-                        <p>{{ __('users.city') }}: {{ $user->city?:'Not Provided' }}</p>
-                        <p>{{ __('users.county') }}: {{ $user->county?:'Not Provided' }}</p>
-                        <p>{{ __('users.country') }}: {{ $user->country?:'Not Provided' }}</p>
-                        <p>{{ __('users.post_code') }}: {{ $user->post_code }}</p>
-                        <p>{{ __('users.full_or_part') }}: {{ $user->full_or_part }}</p>
-                    </div>
-                    <div class="float-right">
-                        <p>{{ __('users.phone') }}: {{ $user->phone }}</p>
-                        <p>{{ __('users.salary') }}: {{$currencySymbol}}{{ number_format($user->salary,2) }}</p>
-                        <p>{{ __('users.department') }}: {{ $user->department->name }}</p>
-                        <p>{{ __('users.role') }}: {{ $user->role->name }}</p>
-                        <p>{{ __('users.seniority') }}: {{ $user->seniority->name }}</p>
-                        <p>{{ __('users.job') }}: {{ $user->job->name }}</p>
-                        <p>{{ __('users.holiday_entitlement') }}: {{ $user->holidayEntitlement->total }}</p>
-                        <p>{{ __('users.contact') }}: {{ $user->contact->name }}</p>
+                    <div class="row">
+                        <div class="col-md-6">
+                            @if(isset($user) && $user->profile_picture)
+                                <img src="{{ asset('storage/' . $user->profile_picture) }}" alt="profile_picture" class="profile-picture-circle">
+                            @endif
+                            <p>{{ __('users.first_line') }}: {{ $user->first_line }}</p>
+                            <p>{{ __('users.second_line') }}: {{ $user->second_line?:'Not Provided' }}</p>
+                            <p>{{ __('users.town') }}: {{ $user->town?:'Not Provided' }}</p>
+                            <p>{{ __('users.city') }}: {{ $user->city?:'Not Provided' }}</p>
+                            <p>{{ __('users.county') }}: {{ $user->county?:'Not Provided' }}</p>
+                            <p>{{ __('users.country') }}: {{ $user->country?:'Not Provided' }}</p>
+                            <p>{{ __('users.post_code') }}: {{ $user->post_code }}</p>
+                            <p>{{ __('users.full_or_part') }}: {{ $user->full_or_part }}</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p>{{ __('users.phone') }}: {{ $user->phone }}</p>
+                            <p>{{ __('users.salary') }}: {{$currencySymbol}}{{ number_format($user->salary,2) }}</p>
+                            <p>{{ __('users.department') }}: {{ $user->department->name }}</p>
+                            <p>{{ __('users.role') }}: {{ $user->role->name }}</p>
+                            <p>{{ __('users.seniority') }}: {{ $user->seniority->name }}</p>
+                            <p>{{ __('users.job') }}: {{ $user->job->name }}</p>
+                            <p>{{ __('users.holiday_entitlement') }}: {{ $user->holidayEntitlement->total }}</p>
+                            <p>{{ __('users.contact') }}: {{ $user->contact->name }}</p>
+                        </div>
+                        @if($canViewSensitiveDocs)
+                            <div class="mt-4">
+                                <h5>{{ __('Sensitive Documents') }}</h5>
+                                <p>{{ __('This section contains sensitive documents related to the user.') }}</p>
+                                @if(isset($user) && $user->cv_path)
+                                    <a href="{{ asset('storage/' . $user->cv_path) }}" target="_blank">Download CV</a>
+                                @endif
+
+                                @if(isset($user) && $user->cover_letter)
+                                    <a href="{{ asset('storage/' . $user->cover_letter) }}" target="_blank">Download Cover Letter</a>
+                                @endif
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
