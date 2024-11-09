713a9724e72c9a0ad959666cfbdc136d99a808c2
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-11-09 10:32:25 +0000

    #105 Update to formatting

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Create a User</div>
+                @include('users.partials.form')
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/users/update.blade.php
+++ resources/views/users/update.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Update {{ $user->name }}</div>
+                @include('users.partials.form')
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
