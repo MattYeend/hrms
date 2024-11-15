b3840b085af87291b016090abbb0bbff8e1de97b
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-11-15 07:45:07 +0000

    #138 en department lang file

---
+++ lang/en/departments.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'create' => 'Create a Department',
+    'edit' => 'Update',
+    'view' => 'View Department',
+    'delete' => 'Delete',
+    'users' => 'Users',
+    'dept_lead' => 'Department Lead',
+    'company' => 'Company'
+];
