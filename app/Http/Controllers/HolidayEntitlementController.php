7a07989813140dbc40b2be7bff53d1b83468971e
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-11-22 09:06:55 +0000

    #166 Added middleware to more controllers

--- app/Http/Controllers/HolidayEntitlementController.php
+++ app/Http/Controllers/HolidayEntitlementController.php
@@ -11,8 +11,7 @@ class HolidayEntitlementController extends Controller
 {
     public function __construct()
     {
-        // Add Middleware to protect routes
-        // E.G $this->middleware('auth');
+        $this->middleware('auth');
     }
     
     /**
