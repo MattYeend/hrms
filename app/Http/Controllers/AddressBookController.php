5ec53c2af7f78e671e7aad297ca7cbf3ac9d16c3
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-11-22 09:03:41 +0000

    #166 Update to first few controllers

--- app/Http/Controllers/AddressBookController.php
+++ app/Http/Controllers/AddressBookController.php
@@ -11,8 +11,7 @@ class AddressBookController extends Controller
 {
     public function __construct()
     {
-        // Add Middleware to protect routes
-        // E.G $this->middleware('auth');
+        $this->middleware('auth');
     }
     
     /**
