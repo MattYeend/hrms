25fc96a7f42872239f592e4e7d4ea12a8f61f5e5
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-11-29 08:17:02 +0000

    #153 Changes to store and update requests for licences

--- app/Http/Requests/StoreLicenceRequest.php
+++ app/Http/Requests/StoreLicenceRequest.php
@@ -11,7 +11,7 @@ class StoreLicenceRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return auth()->user()->isSuperAdmin();
     }
 
     /**
@@ -22,7 +22,8 @@ class StoreLicenceRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'price' => 'required|string|max:10',
         ];
     }
 }
--- app/Http/Requests/UpdateLicenceRequest.php
+++ app/Http/Requests/UpdateLicenceRequest.php
@@ -11,7 +11,7 @@ class UpdateLicenceRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return auth()->user()->isSuperAdmin();
     }
 
     /**
@@ -22,7 +22,8 @@ class UpdateLicenceRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'price' => 'required|string|max:10',
         ];
     }
 }
