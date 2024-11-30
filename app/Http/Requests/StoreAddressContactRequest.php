e501b24ead8f8b688b4cc066879ad09cac7627c8
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-11-30 11:29:41 +0000

    #157 Update to requests

--- app/Http/Requests/StoreAddressContactRequest.php
+++ app/Http/Requests/StoreAddressContactRequest.php
@@ -11,7 +11,7 @@ class StoreAddressContactRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return auth()->user()->isSuperAdmin();
     }
 
     /**
@@ -22,7 +22,12 @@ class StoreAddressContactRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'phone' => 'nullable|string|max:255',
+            'position' => 'required|string|max:255',
+            'main_contact' => 'nullable|integer',
+            'secondary_contact' => 'nullable|integer',
         ];
     }
 }
--- app/Http/Requests/UpdateAddressContactRequest.php
+++ app/Http/Requests/UpdateAddressContactRequest.php
@@ -11,7 +11,7 @@ class UpdateAddressContactRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return auth()->user()->isSuperAdmin();
     }
 
     /**
@@ -22,7 +22,12 @@ class UpdateAddressContactRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'phone' => 'nullable|string|max:255',
+            'position' => 'required|string|max:255',
+            'main_contact' => 'nullable|integer',
+            'secondary_contact' => 'nullable|integer',
         ];
     }
 }
