5cfec773e3751b88f977722c120de10293982bdb
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-12-01 16:14:18 +0000

    #201 Added rules to contract requests

--- app/Http/Requests/StoreContractRequest.php
+++ app/Http/Requests/StoreContractRequest.php
@@ -11,7 +11,7 @@ class StoreContractRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return auth()->user()->isSuperAdmin();
     }
 
     /**
@@ -22,7 +22,10 @@ class StoreContractRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'start' => 'required|date',
+            'end' => 'required|date',
+            'licence_id' => 'required|exists:licence,id',
         ];
     }
 }
--- app/Http/Requests/UpdateContractRequest.php
+++ app/Http/Requests/UpdateContractRequest.php
@@ -11,7 +11,7 @@ class UpdateContractRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return auth()->user()->isSuperAdmin();
     }
 
     /**
@@ -22,7 +22,10 @@ class UpdateContractRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'start' => 'required|date',
+            'end' => 'required|date',
+            'licence_id' => 'required|exists:licence,id',
         ];
     }
 }
