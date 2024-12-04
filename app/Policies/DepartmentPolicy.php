68655d7ab3b6b8d8845ad1e78a05ed39ab421bc6
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-12-04 08:04:07 +0000

    #13 Update to department policy

--- app/Policies/DepartmentPolicy.php
+++ app/Policies/DepartmentPolicy.php
@@ -8,12 +8,23 @@ use Illuminate\Auth\Access\Response;
 
 class DepartmentPolicy
 {
+        /**
+     * Perform preliminary checks before any other authorization methods.
+     * Grants all permissions to super admins, admins and C Suite staff.
+     */
+    public function before(User $user)
+    {
+        if ($user->isSuperAdmin() || $user->isAdmin() || $user->cSuite()) {
+            return true;
+        }
+    }
+
     /**
      * Determine whether the user can view any models.
      */
     public function viewAny(User $user): bool
     {
-        //
+        return false;
     }
 
     /**
@@ -21,7 +32,7 @@ class DepartmentPolicy
      */
     public function view(User $user, department $department): bool
     {
-        //
+        return false;
     }
 
     /**
@@ -29,7 +40,7 @@ class DepartmentPolicy
      */
     public function create(User $user): bool
     {
-        //
+        return false;
     }
 
     /**
@@ -37,7 +48,7 @@ class DepartmentPolicy
      */
     public function update(User $user, department $department): bool
     {
-        //
+        return false;
     }
 
     /**
@@ -45,7 +56,7 @@ class DepartmentPolicy
      */
     public function delete(User $user, department $department): bool
     {
-        //
+        return false;
     }
 
     /**
@@ -53,7 +64,7 @@ class DepartmentPolicy
      */
     public function restore(User $user, department $department): bool
     {
-        //
+        return false;
     }
 
     /**
@@ -61,6 +72,6 @@ class DepartmentPolicy
      */
     public function forceDelete(User $user, department $department): bool
     {
-        //
+        return false;
     }
 }
