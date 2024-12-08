74430d9399cc2f9e54037eded27d5a614ed0eef9
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-12-08 08:38:00 +0000

    #230 Update to licences form

--- resources/views/licence/partials/form.blade.php
+++ resources/views/licence/partials/form.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ isset($licence) ? route('lice$licence.update', $licence->id) : route('licence.store') }}" method="POST" enctype="multipart/form-data">
+<form action="{{ isset($licence) ? route('licence.update', $licence->id) : route('licence.store') }}" method="POST" enctype="multipart/form-data">
     @csrf
     @if(isset($licence))
         @method('PUT')
@@ -10,7 +10,8 @@
                 <label for="name">{{ __('licences.name') }} @if(!isset($licence))<span class="text-danger">*</span>@endif</label>
                 <input type="text" name="name" id="name"class="form-control" value="{{ old('name', $licence->name ?? '') }}" required>
             </div>
-
+        </div>
+        <div class="col-md-6">
             <div class="mb-3">
                 <label for="price">{{ __('licences.price') }} @if(!isset($licence))<span class="text-danger">*</span>@endif</label>
                 <input type="text" name="price" id="price"class="form-control" value="{{ old('price', $licence->price ?? '') }}" required>
@@ -20,6 +21,6 @@
 
     <!-- Submit Button -->
     <div class="text-center mt-4">
-        <button class="btn btn-primary" type="submit">{{ isset($licence) ? __('licences.update') . ' ' . $licence->name : __('licences.create') }}</button>
+        <button class="btn btn-primary" type="submit">{{ isset($licence) ? __('licences.edit') . ' ' . $licence->name : __('licences.create') }}</button>
     </div>
 </form>
