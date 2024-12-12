784d03a831909037ef647423ca1a89329b5e1c4f
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-12-12 08:04:41 +0000

    #186 Update to rota show view

--- resources/views/rotas/show.blade.php
+++ resources/views/rotas/show.blade.php
@@ -14,7 +14,14 @@
                         </div>
                     @endif
                     <div class="row">
-                        <div class="col-md-6"></div>
+                        <div class="col-md-6">
+                            <p>{{ __('rotas.user') }}: {{ $rota->user->getFullNameLong() }}</p>
+                            <p>{{ __('rotas.department') }}: {{ $rota->department->name ?? __('rotas.not_provided') }}</p>
+                        </div>
+                        <div class="col-md-6">
+                            <p>{{ __('rotas.start_time') }}: {{ \Carbon\Carbon::parse($rota->start_time)->format('d/m/y H:i') }}</p>
+                            <p>{{ __('rotas.end_time') }}: {{ \Carbon\Carbon::parse($rota->end_time)->format('d/m/y H:i') }}</p>
+                        </div>
                     </div>
                 </div>
             </div>
