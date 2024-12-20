77e893672f8555b760e7a954d2676686a91c0023
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-12-20 08:07:20 +0000

    #16 New -a for email logs

---
+++ app/Http/Controllers/EmailLogsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EmailLogs;
+use App\Http\Requests\StoreEmailLogsRequest;
+use App\Http\Requests\UpdateEmailLogsRequest;
+
+class EmailLogsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEmailLogsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EmailLogs $emailLogs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(EmailLogs $emailLogs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEmailLogsRequest $request, EmailLogs $emailLogs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EmailLogs $emailLogs)
+    {
+        //
+    }
+}
