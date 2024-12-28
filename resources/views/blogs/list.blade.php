dee26a948b2d6ff914879946489c0bc4ec68b336
Matt Yeend <mattyeend1@hotmail.co.uk>
2024-12-28 11:50:35 +0000

    #278 Update to other blog blade files

--- resources/views/blogs/list.blade.php
+++ resources/views/blogs/list.blade.php
@@ -1,16 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ __('blogs.all_blogs') }}</h1>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('blogs.all_blogs') }}</div>
 
-    @foreach($blogs as $blog)
-        <div class="card mt-3">
-            <div class="card-body">
-                <h3>{{ $blog->title }}</h3>
-                <h3>{{ $blog->blogType->name }}</h3>
-                <p>{{ Str::limit($blog->content, 150) }}</p>
-                <a href="{{ route('blogs.show', $blog->id) }}" class="btn btn-info">{{ __('blogs.read_more') }}</a>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <div class="row">
+                        @if(count($blogs) > 0)
+                            @foreach($blogs as $blog)
+                                <div class="card mt-3">
+                                    <div class="card-body">
+                                        <h3>{{ $blog->title }}</h3>
+                                        <h3>{{ $blog->blogType->name }}</h3>
+                                        <p>{{ Str::limit($blog->content, 150) }}</p>
+                                        <a href="{{ route('blogs.show', $blog->id) }}" class="btn btn-info">{{ __('blogs.read_more') }}</a>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="6">{{ __('blogs.no_blogs') }}</td>
+                            </tr>
+                        @endif
+                    </div>
+                </div>
             </div>
         </div>
-    @endforeach
+    </div>
+</div>
 @endsection
--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -1,17 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $blog->title }}</h1>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ $blog->title }}</div>
 
-    <div>
-        <p><strong>{{ __('blogs.status') }}:</strong> {{ $blog->status }}</p>
-        <p><strong>{{ __('blogs.approval_status') }}:</strong> {{ $blog->approval_status }}</p>
-        <p><strong>{{ __('blogs.blog_type') }}:</strong> {{ $blog->blogType->name }}</p>
-        <p><strong>{{ __('blogs.content') }}:</strong></p>
-        <p>{{ $blog->content }}</p>
-        <p><strong>{{ __('blogs.author') }}:</strong> {{ $blog->author->name }}</p>
-        @if($blog->approval_status == 'approved')
-            <a href="{{ route('blogs.list') }}" class="btn btn-primary">{{ __('blogs.back_to_list') }}</a>
-        @endif
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <div class="row">
+                        <p><strong>{{ __('blogs.status') }}:</strong> {{ $blog->status }}</p>
+                        <p><strong>{{ __('blogs.approval_status') }}:</strong> {{ $blog->approval_status }}</p>
+                        <p><strong>{{ __('blogs.blog_type') }}:</strong> {{ $blog->blogType->name }}</p>
+                        <p><strong>{{ __('blogs.content') }}:</strong></p>
+                        <p>{{ $blog->content }}</p>
+                        <p><strong>{{ __('blogs.author') }}:</strong> {{ $blog->author->name }}</p>
+                        @if($blog->approval_status == 'approved')
+                            <a href="{{ route('blogs.list') }}" class="btn btn-primary">{{ __('blogs.back_to_list') }}</a>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
 @endsection
