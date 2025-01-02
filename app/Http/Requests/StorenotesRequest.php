45a59b52383427d48ea63dfd61a45293093105ee
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-01-02 12:22:06 +0000

    #19 Update to notes requests and policy

--- app/Http/Requests/StorenotesRequest.php
+++ app/Http/Requests/StorenotesRequest.php
@@ -11,7 +11,7 @@ class StoreNotesRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return auth()->check();
     }
 
     /**
@@ -22,7 +22,9 @@ class StoreNotesRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'note_type_id' => 'required|exists:note_types,id',
         ];
     }
 }
--- app/Policies/NotesPolicy.php
+++ app/Policies/NotesPolicy.php
@@ -13,7 +13,7 @@ class NotesPolicy
      */
     public function viewAny(User $user): bool
     {
-        //
+        return $user->isSuperAdmin() || $user->isAdmin() || $user->hrStaff();
     }
 
     /**
@@ -21,7 +21,7 @@ class NotesPolicy
      */
     public function view(User $user, Notes $notes): bool
     {
-        //
+        return $user->isSuperAdmin() || $user->isAdmin() || $user->hrStaff() || $user->id === $note->created_by;
     }
 
     /**
@@ -29,7 +29,7 @@ class NotesPolicy
      */
     public function create(User $user): bool
     {
-        //
+        return $user->isSuperAdmin() || $user->isAdmin() || $user->hrStaff();
     }
 
     /**
@@ -37,7 +37,7 @@ class NotesPolicy
      */
     public function update(User $user, Notes $notes): bool
     {
-        //
+        return $user->isSuperAdmin() || $user->isAdmin() || $user->id === $note->created_by;
     }
 
     /**
@@ -45,7 +45,7 @@ class NotesPolicy
      */
     public function delete(User $user, Notes $notes): bool
     {
-        //
+        return $user->isSuperAdmin() || $user->isAdmin();
     }
 
     /**
@@ -53,7 +53,7 @@ class NotesPolicy
      */
     public function restore(User $user, Notes $notes): bool
     {
-        //
+        return false;
     }
 
     /**
@@ -61,6 +61,6 @@ class NotesPolicy
      */
     public function forceDelete(User $user, Notes $notes): bool
     {
-        //
+        return false;
     }
 }
